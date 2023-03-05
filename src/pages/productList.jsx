aa96d35561cd8722ec7c9310105778e7be97c4d9
Lisa ^^ <islahola>
2023-03-05 20:05:31 +0700

    revisi

--- src/pages/productList.jsx
+++ src/pages/productList.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import ListProduct from '../component/listProduct';
 import Navbar from '../component/navbar';
+import { getDataProdctCloud } from '../redux/action/listProduct';
 
 const ProductList = () => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        const unsicribe = () =>{
+            dispatch(getDataProdctCloud())
+        } 
+        return () => {
+            unsicribe()
+        };
+    }, []);
     return (
         <div>
+            
             <ListProduct/>
         </div>
     );
