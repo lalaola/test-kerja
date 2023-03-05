432c84567ef2bfc330faaf8feb0dcd89c4809294
Lisa ^^ <islahola>
2023-03-05 19:49:14 +0700

    revisi

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -6,7 +6,7 @@ const Homepage = () => {
         <div className=' home '>
             <div className=" text col-8 col-md-6"> <Hero /></div>
             <div className="gambar  col-md-6 col-4 bg-danger d-flex ">
-                <div className="col-12 pe-4 text col-md-5 align-items-end mb-5 ">
+                <div className="col-12 d-flex pe-4 text col-md-5 align-items-end mb-5 ">
                     <p className='col-12 ps-4 text-end'> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis neque repellendus quibusdam! Aliquid sapiente perferendis eius, non ad error amet officiis, odit numquam quis animi dolorem architecto ipsa nesciunt dolor?
                     </p>
                 </div>
